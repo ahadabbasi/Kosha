1abd70b1b24ba557090b1bdd7aeac74221680449
ahadabbasi <ahadabbasi@gmail.com>
2025-12-19 19:34:44 +0330

    Install NuGet

--- src/CustomerManager/Kosha.CustomerManager.WindowsClient/App.xaml.cs
+++ src/CustomerManager/Kosha.CustomerManager.WindowsClient/App.xaml.cs
@@ -1,14 +1,19 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Kosha.CustomerManager.WindowsClient
+namespace Kosha.CustomerManager.WindowsClient;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public App()
     {
+
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+    }
 }
