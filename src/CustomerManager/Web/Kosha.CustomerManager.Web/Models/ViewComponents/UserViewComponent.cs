1c665202046cc0427945bcd24601b535493615c9
ahadabbasi <ahadabbasi@gmail.com>
2026-06-26 19:33:16 +0330

    Fix the user dropdown issues

---
+++ src/CustomerManager/Web/Kosha.CustomerManager.Web/Models/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Kosha.CustomerManager.Web.Infrastructure.Helper.Authentication;
+using Kosha.CustomerManager.Web.Infrastructure.Models.Authentication;
+using Kosha.CustomerManager.Web.Shared.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kosha.CustomerManager.Web.Models.ViewComponents;
+
+public sealed class UserViewComponent(IAuthorizeService authorizeService) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        Result<AuthorizationResponse> resultAuthenticate = 
+            authorizeService.Authenticate();
+
+        return resultAuthenticate && resultAuthenticate.Data is not null
+            ? View(resultAuthenticate.Data)
+            : Content(string.Empty);
+    }
+}
